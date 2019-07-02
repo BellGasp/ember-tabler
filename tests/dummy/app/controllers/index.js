f80a10b97cd246139dd5df7afa3a696877102266
Guillaume Laj <guillaume.lajambe@gmail.com>
2019-07-02 15:28:39 -0400

    Add-basic-style
    -Adds ember-cli-sass
    -Imports in dummy
    -Adds basic paginations layout
    -Adds actions for fetchRecords/ pagination

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,8 +1,30 @@
 import Controller from '@ember/controller';
-import { alias } from '@ember/object/computed';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  store: service(),
 
-  characters: alias('model'),
+  characters: null,
+
+  init() {
+    this._super(...arguments);
+
+    //TEMPOO
+    this.fetchRecords();
+  },
+
+  async fetchRecords(){
+    let characters = await this.store.findAll('character');
+    this.set('characters', characters);
+  },
+
+  actions:{
+    fetchRecords(){
+      this.fetchRecords();
+    },
+    goToPage(page){
+
+    }
+  }
 
 });
